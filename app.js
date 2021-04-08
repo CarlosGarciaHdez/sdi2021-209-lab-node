ffb06cb11b0c6a6e9c0d7081f1565923b920cb67
Carlos <carlosgarciahdez19@gmail.com>
2021-04-08 10:27:02 +0200

    SDI-2021-209-8.1-Acceso a datos de Mongo desde Node.js.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let app = express();
 /*
  mongodb://admin:sdi@tiendamusica-shard-00-00.yqsut.mongodb.net:27017,tiendamusica-shard-00-01.yqsut.mongodb.net:27017,tiendamusica-shard-00-02.yqsut.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-10iqbi-shard-0&authSource=admin&retryWrites=true&w=majority
  */
+let mongo = require('mongodb');
 let swig = require('swig');
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -13,10 +14,12 @@ app.use(express.static('public'));
 
 // Variables
 app.set('port', 8081);
+app.set('db', 'mongodb://admin:sdi@tiendamusica-shard-00-00.yqsut.mongodb.net:27017,tiendamusica-shard-00-01.yqsut.mongodb.net:27017,tiendamusica-shard-00-02.yqsut.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-10iqbi-shard-0&authSource=admin&retryWrites=true&w=majority');
+
 
 //Rutas/controladores por lógica
 require("./routes/rusuarios.js")(app, swig); // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app, swig); // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app, swig, mongo); // (app, param1, param2, etc.)
 require("./routes/rautores.js")(app, swig); // (app, param1, param2, etc.)
 
 //Lanzar el servidor
